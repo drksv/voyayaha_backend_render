15142b8812fed56371f56900ac52a09aa96252dc
drksv <115434390+drksv@users.noreply.github.com>
2026-09-15 21:34:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ app = FastAPI(title="Voyayaha Travel API", version=APP_VERSION)
 DEFAULT_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://10.0.0.183:8080",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
     "https://voyayaha.com",
